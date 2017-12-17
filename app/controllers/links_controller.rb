56c194363440b8ac9684048788b4dbc043177544
范鼎 <zichuange@gmail.com>
2017-12-17 19:41:57 +0800

    Allow unauthorized user access newest links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,5 @@
 class LinksController < ApplicationController
-  before_action :prevent_unauthorized_user_access, except: [:show, :index]
+  before_action :prevent_unauthorized_user_access, except: %i[show index newest]
 
   def index
     @links = Link.hottest
